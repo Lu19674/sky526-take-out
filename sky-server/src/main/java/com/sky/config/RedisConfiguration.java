e1614e48b7d399aaed66fa05a39b3924b0995fa5
Lu_qing195 <930685283@qq.com>
2024-06-10 18:56:26 +0800

    day5 添加Redis的RedisTemplate模板对象各项序列化的配置

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -12,13 +13,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfiguration {
 
     @Bean
-    public RedisTemplate/*redis模板对象*/ redisTemplate(RedisConnectionFactory /*连接工厂对象*/ redisConnectionFactory){
+    @SuppressWarnings("all")//忽略所有警告
+    public RedisTemplate<Object,Object>/*redis模板对象*/ redisTemplate(RedisConnectionFactory /*连接工厂对象*/ redisConnectionFactory){
         log.info("开始创建 Redis 模板对象...");
-        RedisTemplate redisTemplate =new RedisTemplate();
+        RedisTemplate<Object,Object> redisTemplate =new RedisTemplate<>();
         //设置redis连接工厂对象
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        //设置redis key前序列化器
+        //设置redis 各项数据类型的前序列化器
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 }
